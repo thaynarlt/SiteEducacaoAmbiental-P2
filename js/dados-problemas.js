5a84a64fcec720de74ae2006f9337551577d083b
Thayna Tolentino <tatarlt7@gmail.com>
2023-10-12 09:38:37 -0300

    commit

---
+++ js/dados-problemas.js
@@ -0,0 +1,22 @@
+const problemas = [
+    {
+        id: 1,
+        titulo: 'Acesso à Informação',
+        descricao: 'A falta de educação ambiental deixa as pessoas sem informações vitais sobre a proteção do planeta.',
+        imagem: './images/vetor-informacao.svg'
+    },
+    {
+        id: 2,
+        titulo: 'Falta de Conscientização',
+        descricao: 'Sem educação ambiental, a ignorância impera e os danos à natureza se multiplicam.',
+        imagem: './images/vetor-pensamento.svg'
+    },
+    {
+        id: 3,
+        titulo: 'Lixo e Poluição',
+        descricao: 'Cada gesto negligente impacta o nosso lar comum.',
+        imagem: './images/vetor-lixo.svg'
+    }
+];
+
+export default problemas;
---
+++ js/main.js
@@ -0,0 +1,15 @@
+// 1) obter os objetos
+import problemas from './dados-problemas.js';
+import createCardView from './components/cards-problemas.js';
+
+// Obter div de visualização dos cards
+const containerProblemas = document.querySelector('#cards-problemas');
+
+
+const renderProblemas = (problemas) => {
+    containerProblemas.innerHTML = problemas
+      .map((problema) => createCardView(problema))
+      .join('');
+};
+  
+renderProblemas(problemas);
