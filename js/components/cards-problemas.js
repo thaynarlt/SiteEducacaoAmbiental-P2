5cc97127ccbcf2de9bf78473dd98b91061a7443e
l-e-t-i-c-i-a <proj3705@gmail.com>
2023-10-23 19:51:24 -0300

    sombreado cards

--- js/components/cards-problemas.js
+++ js/components/cards-problemas.js
@@ -1,6 +1,6 @@
 function createCardView(problemas) {
     return `
-        <div id="problema-${problemas.id}" style="    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25); width: 333px; height: 313px; border-radius: 20px; background: var(--White, #FFF); display: flex; justify-content: center;align-items: center; margin: 8px 10px;">
+        <div id="problema-${problemas.id}" style="    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25); width: 333px; height: 313px; border-radius: 20px; background: var(--White, #FFF); display: flex; justify-content: center;align-items: center; margin: 8px 10px;" class="sombra">
             <div style="display: flex; flex-direction: column; flex-wrap: wrap; align-items: center; width: 80%;">
             <img src="${problemas.imagem}" alt="">
             <h3 style="color: var(--Grey, #393E46);
